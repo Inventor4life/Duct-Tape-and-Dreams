35f66f88868f2ac9e09290eeea3d862499e85066
Bio <144750331+Bioticcc@users.noreply.github.com>
2024-10-21 20:00:32 -0700

    Updated again

--- Duct-Tape-and-Dreams/Duct-Tape-and-Dreams/main.cpp
+++ Duct-Tape-and-Dreams/Duct-Tape-and-Dreams/main.cpp
@@ -5,7 +5,8 @@ int main(int argc, char* argv[]) {
 	sol::state lua;
 	lua.open_libraries(sol::lib::base);
 
-	lua.script("print(_VERSION)");
+	//lua.script("print(_VERSION)");
+	lua.script("if type(jit) == 'table' then print(jit.version) end");
 
 	//lua.script("source.lua");
 
