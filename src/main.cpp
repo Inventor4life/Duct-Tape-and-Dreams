91b4b1dee5b9f74c23fb018b04d4e22b7e90b8b2
Ethan <93846658+Inventor4life@users.noreply.github.com>
2025-03-26 21:51:00 -0700

    Updated CMakeLists.txt
    Added sol2/Lua/LuaJIT install capability to CMakeLists.txt. Updated TODO.md with more tasks (probably should have been updated in separate commit).

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
+#include <iostream>
+#include <sol/sol.hpp>
 #include <steam/steam_api.h>
 
 int main()
 {
+	// Verify SteamAPI installation
 	if(!SteamAPI_Init()) {
 	return -1;
 	}
 	SteamAPI_Shutdown();
+
+	// Verify sol2/Lua/LuaJIT installation
+	sol::state lua;
+	lua.open_libraries(sol::lib::jit, sol::lib::base);
+	lua.script("print(jit.version)");
+
 	return 0;
 }
